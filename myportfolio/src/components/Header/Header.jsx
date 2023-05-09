394fcdc7d6a8e6a2792bd5314e8a92ffee36ac1c
JasonKim0122 <JasonKim0122@gmail.com>
2023-05-09 17:55:27 -0500

    added headers and created buttons

--- myportfolio/src/components/Header/Header.jsx
+++ myportfolio/src/components/Header/Header.jsx
@@ -10,6 +10,15 @@ const Header = () => {
                     <h2 className='text-uppercase text-white op-07 fw-6 ls-2'>
                         Web design
                     </h2>
+                    <h1 className='text-white fw-4 header-title'>
+                        Hello my name is Jason an aspiring
+                        <span className='text-primary'> Full Stack Developer </span> 
+                        living in Plano, Texas
+                    </h1>
+                    <div className='btn-groups flex'>
+                        <button type='button' className='btn-item bg-primary fw-3 ls-2'>See Work</button>
+                        <button type='button' className='btn-item bg-secondary fw-3 ls-2'>Contact Me</button>
+                    </div>
                 </div>
             </div>
         </div>
