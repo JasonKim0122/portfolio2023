f60f0f2c48d6c2402fd3a4305e48e5599883b5f1
JasonKim0122 <JasonKim0122@gmail.com>
2023-05-10 15:46:28 -0500

    added services images along with css

--- myportfolio/src/constants/data.js
+++ myportfolio/src/constants/data.js
@@ -5,6 +5,16 @@ const services = [
         image: `${images.services_img_1}`,
         title: 'Web Development',
         paragraph: 'Hello I develop websites'
+    },
+    {
+        image: `${images.services_img_2}`,
+        title: 'Front-End',
+        paragraph: 'Front end'
+    },
+    {
+        image: `${images.services_img_3}`,
+        title: 'Back-End',
+        paragraph: 'Back-end'
     }
 ];
 
--- myportfolio/src/constants/images.js
+++ myportfolio/src/constants/images.js
@@ -1,9 +1,11 @@
 import header_background from '../assets/images/background-header.jpg';
-import services_img_1 from '../assets/images/services_img_1.PNG';
-import services_img_2 from '../assets/images/services_img_2.PNG';
+import services_img_1 from '../assets/images/services_img_1.svg';
+import services_img_2 from '../assets/images/services_img_2.svg';
+import services_img_3 from '../assets/images/services_img_3.svg'; 
+import services_img_4 from '../assets/images/services_img_4.PNG'; 
 
 const images = {
-    header_background, services_img_1, services_img_2
+    header_background, services_img_1, services_img_2, services_img_3, services_img_4
 }
 
 export default images
