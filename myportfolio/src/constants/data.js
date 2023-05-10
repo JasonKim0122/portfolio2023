1204067b9773ca8a9d5a68ed571fa53ee05d1654
JasonKim0122 <JasonKim0122@gmail.com>
2023-05-09 20:11:21 -0500

    created services tab

---
+++ myportfolio/src/constants/data.js
@@ -0,0 +1,11 @@
+import images from './images';
+
+const services = [
+    {
+        image: `${images.services_img_1}`,
+        title: 'Web Development',
+        paragraph: 'Hello I develop websites'
+    }
+];
+
+export {services};
---
+++ myportfolio/src/constants/images.js
@@ -0,0 +1,9 @@
+import header_background from '../assets/images/background-header.jpg';
+import services_img_1 from '../assets/images/services_img_1.PNG';
+import services_img_2 from '../assets/images/services_img_2.PNG';
+
+const images = {
+    header_background, services_img_1, services_img_2
+}
+
+export default images
